8daac69e67488d31dd5fe8b40bb4493e5a0bb177
sundarleo11 <sundarrock1999@gmail.com>
2022-07-27 15:25:14 +0530

    Setting Attributes

--- counter-app/src/component/counter.jsx
+++ counter-app/src/component/counter.jsx
@@ -4,19 +4,28 @@ class counter extends Component {
     state = {
         count: 0
     }
+
+    style = {
+        fontSize: 10,
+        fontWeight: "Bold",
+
+    }
     render() {
         return <React.Fragment>
-            <h1>Helloworld</h1>
-            {/* <span>{this.state.count}</span>*/}
-            <span>{this.formatecount()}</span>
-            <button>Increament</button>
+
+            {/* <span>{this.state.count}</span>
+               <h1>Helloworld</h1>
+               style={this.style} 
+            */}
+            <span className='badge badge-primary m-2'>{this.formatecount()}</span>
+            <button className='btn btn-secondary  btn-sm'>Increament</button>
         </React.Fragment>
     }
 
     formatecount() {
         // const { counter } = this.state;
         const { count } = this.state;
-        return count === 0 ? <h4>zero</h4> : count;
+        return count === 0 ? "Zero" : count;
     }
 }
 
