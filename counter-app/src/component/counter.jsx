585ba2679645791e8a88bb46124f84c0a9f707bf
sundarleo11 <sundarrock1999@gmail.com>
2022-07-30 11:07:19 +0530

    Conditional Rendering

--- counter-app/src/component/counter.jsx
+++ counter-app/src/component/counter.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class counter extends Component {
     state = {
         count: 1,
-        tag: ['tag1', 'tag2', 'tag3']
+        tag: []
     }
 
     style = {
@@ -11,30 +11,37 @@ class counter extends Component {
         fontWeight: "Bold",
 
     }
+
+    renderTag() {
+        if (this.state.tag.length === 0) return <p>There are No Tags</p>;
+
+        return <ul>{this.state.tag.map(tag => <li key={tag}>{tag}</li>)}</ul>
+    }
     render() {
 
-        let classes = "badge m-2 badge-";
-        classes += this.state.count === 0 ? "warning" : "primary";
+        /*  let classes = "badge m-2 badge-";
+          classes += this.state.count === 0 ? "warning" : "primary";*/
 
-        return <React.Fragment>
+        return <div>{this.renderTag()}</div>
 
-            {/* <span>{this.state.count}</span>
-               <h1>Helloworld</h1>
-               style={this.style} 
-            */}
-            <span className={classes}>{this.formatecount()}</span>
-            <button className='btn btn-secondary  btn-sm'>Increament</button>
-            <ul>
-                {this.state.tag.map(tag => <li key={tag}>{tag}</li>)}
-            </ul>
-        </React.Fragment>
+        /* return <React.Fragment>
+ 
+ 
+             <span>{this.state.count}</span>
+                <h1>Helloworld</h1>
+                style={this.style} 
+             
+              <span className={classes}>{this.formatecount()}</span>
+             <button className='btn btn-secondary  btn-sm'>Increament</button>
+            
+         </React.Fragment>*/
     }
 
-    formatecount() {
-        // const { counter } = this.state;
-        const { count } = this.state;
-        return count === 0 ? "Zero" : count;
-    }
+    /* formatecount() {
+         // const { counter } = this.state;
+         const { count } = this.state;
+         return count === 0 ? "Zero" : count;
+     }*/
 }
 
 export default counter;
